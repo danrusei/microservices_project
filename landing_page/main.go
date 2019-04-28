7ea25581efd18567ef825bb528aeb124e814fccb
Dan Rusei <dan.rusei@gmail.com>
2019-04-28 19:05:30 +0300

    starting landing page

---
+++ landing_page/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO List",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Printf("The server can't start %v", err)
+	}
+}
