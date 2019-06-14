f586d16c4c97929b2ec692c4b056bb92ad5d1314
Dan Rusei <dan.rusei@gmail.com>
2019-06-14 13:58:01 +0300

    Stats Service working on grpc

--- stats/transport/grpc.go
+++ stats/transport/grpc.go
@@ -33,4 +33,28 @@ func NewGRPCServer(newendpoints endpoints.Endpoints, logger log.logger) pb.Stats
 	}
 }
 
-func (s *gRPCServer) ListTable(ctx context.Context, req *pb.TableRequest)
+func (s *gRPCServer) ListTable(ctx context.Context, req *pb.TableRequest) (*pb.TableReply, error) {
+	_, resp, err := s.listTable.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.TableReply), nil
+}
+
+func (s *gRPCServer) ListTeamPlayers(ctx context.Context, req *pb.TeamRequest) (*pb.TeamReply, error) {
+	_, resp, err := s.listTeamPlayers.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.TeamReply), nil
+}
+
+func (s *gRPCServer) ListPositionPlayers(ctx context.Context, req *pb.PositionRequest) (*pb.PositionReply, error) {
+	_, resp, err := s.listPositionPlayers.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.PositionReply), nil
+}
+
+//TODO have to add encode and decode functions !!!!!
