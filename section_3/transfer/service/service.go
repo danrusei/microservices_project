be956d385d0806e5053a080bd61546cde4e0e617
Dan Rusei <dan.rusei@gmail.com>
2019-07-08 15:23:42 +0300

    creating the transfer service

--- section_3/transfer/service/service.go
+++ section_3/transfer/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"errors"
+	"strings"
 
 	"cloud.google.com/go/firestore"
 	"github.com/go-kit/kit/log"
@@ -14,6 +15,12 @@ var (
 
 	//ErrExtractDataToStruct informs if unable to extract firestore data to struct
 	ErrExtractDataToStruct = errors.New("can't extract the data into a struct with DataTo")
+
+	//ErrWrite informs if database write error occurs
+	ErrWrite = errors.New("can't write the document")
+
+	//ErrDelete informs if unable to delete the player
+	ErrDelete = errors.New("can't delete the player from database")
 )
 
 //TransferService  describe the Transfer service
@@ -44,10 +51,59 @@ type basicService struct {
 
 func (s *basicService) TransferPlayer(ctx context.Context, playerTransfer string, TeamFrom string, TeamTo string) (string, error) {
 
+	var teamToTable Table
+	var teamFromTable Table
+	var playerStruct Player
+
+	docDelete := TeamFrom + "_" + strings.Replace(playerTransfer, " ", "_", -1)
+	docCreate := TeamTo + "_" + strings.Replace(playerTransfer, " ", "_", -1)
+
 	//check player price
 	//check teamTO Capital
 	//if price <= Capital then transfer is allowed
 	//delete TeamFrom_player; create TeamTo_player(change Team field)
+	//addjust teams money if transfer is done
+
+	teamTOtransfer, err := s.dbClient.Collection("League").Doc(TeamTo).Get(ctx)
+	if err != nil {
+		return "", err
+	}
+	if err := teamTOtransfer.DataTo(&teamToTable); err != nil {
+		return "", ErrExtractDataToStruct
+	}
+
+	teamFROMtransfer, err := s.dbClient.Collection("League").Doc(TeamFrom).Get(ctx)
+	if err != nil {
+		return "", err
+	}
+	if err := teamFROMtransfer.DataTo(&teamFromTable); err != nil {
+		return "", ErrExtractDataToStruct
+	}
+
+	playerTOtransfer, err := s.dbClient.Collection("Teams").Doc(docDelete).Get(ctx)
+	if err != nil {
+		return "", err
+	}
+	if err := playerTOtransfer.DataTo(&playerStruct); err != nil {
+		return "", ErrExtractDataToStruct
+	}
+
+	if teamToTable.TeamCapital < playerStruct.Price {
+		return "The team can't afford the player", nil
+	}
+
+	_, err = s.dbClient.Collection("Teams").Doc(docDelete).Delete(ctx)
+	if err != nil {
+		return "", ErrDelete
+	}
+
+	_, err = s.dbClient.Collection("Teams").Doc(docCreate).Create(ctx, playerStruct)
+
+	if err != nil {
+		return "", ErrWrite
+	}
+
+	//last to make the Set for each team and modify just Capital
 
 	return "", nil
 }
